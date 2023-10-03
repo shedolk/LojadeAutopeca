034e4415f4db22660a3641ceea8bcfc5a203d50d
henriquebeckmann10@gmail.com <H-Beckmann>
2023-10-03 20:44:28 -0300

    :rotating_light: Teste

--- src/main/java/br/unitins/topicos1/modelo/Cupom.java
+++ src/main/java/br/unitins/topicos1/modelo/Cupom.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 @Entity
 public class Cupom extends DefaultEntity {
     private String codigo;
+    private String novo;
 
     public String getCodigo() {
         return codigo;
     }
+
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }

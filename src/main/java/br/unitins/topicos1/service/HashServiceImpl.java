e5fc177e614ac87be1879c3c04fdf8aeb9f17ee9
Matheus Naves <mnaves.dev@gmail.com>
2023-10-21 18:04:47 -0300

    Impl de hashservices, private e public keys

---
+++ src/main/java/br/unitins/topicos1/service/HashServiceImpl.java
@@ -0,0 +1,50 @@
+package br.unitins.topicos1.service;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class HashServiceImpl implements HashService{
+
+    private String salt = "#blahxyz22";
+
+    private Integer iterationCount = 405;
+
+    private Integer keyLenght = 512;
+
+    @Override
+    public String getHashSenha(String senha) {
+
+        try{
+        byte[] result = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
+            .generateSecret(new PBEKeySpec(senha.toCharArray(),
+            salt.getBytes(), iterationCount, keyLenght)).getEncoded();
+
+            return Base64.getEncoder().encodeToString(result);
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+
+        e.printStackTrace();
+        throw new RuntimeException("Erro ao criar um hash");
+    }
+    
+    }
+
+    public static void main(String[] args) {
+        HashService service = new HashServiceImpl();
+
+        service.getHashSenha("123");
+        service.getHashSenha("123");
+        service.getHashSenha("1234");
+        service.getHashSenha("leandra");
+
+        System.out.println(service.getHashSenha("1234"));
+        System.out.println(service.getHashSenha("leandra"));
+    
+    }
+}
